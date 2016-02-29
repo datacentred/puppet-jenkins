61f720e8949cb32a1630ad225e012298a63db62b
Simon Murray <simon.murray@datacentred.co.uk>
2016-02-29 12:55:22 +0000

    Fix Ubuntu Package Install
    A race due to dpkg deps causes the jenkins service to be attempted to be
    started before the JRE is configured causing a failure.  This explicitly
    installs prerequisite packages before the jenkins server

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,6 +4,16 @@
 #
 class jenkins::install {
 
+  $prerequisites = [
+    'default-jre-headless',
+  ]
+
+  ensure_packages($prerequisites)
+
+  # Race in package dependencies causes jenkins to be started before
+  # java has been full installed and configured
+  Package[$prerequisites] ->
+
   package { 'jenkins':
     ensure => installed,
   }
